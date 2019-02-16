b2f621115e1e5781d8253633a662e42052084cbc
NightCandle <instkffff@gmail.com>
2019-02-16 23:29:19 +0800

    .

--- extend.php
+++ extend.php
@@ -18,7 +18,6 @@ return [
     (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js')
         ->css(__DIR__.'/resources/less/UserBio.less'),
-    new Extend\Locales(__DIR__.'/resources/locale'),
     function (Dispatcher $events) {
         $events->subscribe(Listeners\AddUserBioAttribute::class);
     },
